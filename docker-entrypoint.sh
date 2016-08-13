ab18f6d1d20098b04eef6ec7ef4b123be55e5807
ZerounNet <namedhard@qq.com>
2016-08-14 06:38:13 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -55,91 +55,95 @@ initialize_db () {
 
 			mkdir -p "$DATADIR"
 			chown -R mysql:mysql "$DATADIR"
-
-			echo 'Initializing database'
-			mysql_install_db --user=mysql --datadir="$DATADIR" --rpm
-			echo 'Database initialized'
 			
-			"$@" --skip-networking &
-			pid="$!"
+			if [[ ${CLUSTER} = "BOOTSTRAP" || ${CLUSTER} = "STANDALONE" ]]; then
+				echo 'Initializing database'
+				mysql_install_db --user=mysql --datadir="$DATADIR" --rpm
+				echo 'Database initialized'
+				
+				"$@" --skip-networking &
+				pid="$!"
 
-			mysql=( mysql --protocol=socket -uroot )
+				mysql=( mysql --protocol=socket -uroot )
 
-			for i in {30..0}; do
-				if echo 'SELECT 1' | "${mysql[@]}" &> /dev/null; then
-					break
+				for i in {30..0}; do
+					if echo 'SELECT 1' | "${mysql[@]}" &> /dev/null; then
+						break
+					fi
+					echo 'MySQL init process in progress...'
+					sleep 1
+				done
+				if [ "$i" = 0 ]; then
+					echo >&2 'MySQL init process failed.'
+					exit 1
 				fi
-				echo 'MySQL init process in progress...'
-				sleep 1
-			done
-			if [ "$i" = 0 ]; then
-				echo >&2 'MySQL init process failed.'
-				exit 1
-			fi
 
-			if [ -z "$MYSQL_INITDB_SKIP_TZINFO" ]; then
-				# sed is for https://bugs.mysql.com/bug.php?id=20545
-				mysql_tzinfo_to_sql /usr/share/zoneinfo | sed 's/Local time zone must be set--see zic manual page/FCTY/' | "${mysql[@]}" mysql
-			fi
+				if [ -z "$MYSQL_INITDB_SKIP_TZINFO" ]; then
+					# sed is for https://bugs.mysql.com/bug.php?id=20545
+					mysql_tzinfo_to_sql /usr/share/zoneinfo | sed 's/Local time zone must be set--see zic manual page/FCTY/' | "${mysql[@]}" mysql
+				fi
 
-			"${mysql[@]}" <<-EOSQL
-				-- What's done in this file shouldn't be replicated
-				--  or products like mysql-fabric won't work
-				SET @@SESSION.SQL_LOG_BIN=0;
+				"${mysql[@]}" <<-EOSQL
+					-- What's done in this file shouldn't be replicated
+					--  or products like mysql-fabric won't work
+					SET @@SESSION.SQL_LOG_BIN=0;
 
-				DELETE FROM mysql.user ;
-				CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
-				GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
-				DROP DATABASE IF EXISTS test ;
-				FLUSH PRIVILEGES ;
-			EOSQL
-			
-			if [ ! -z "$MYSQL_ROOT_PASSWORD" ]; then
-				mysql+=( -p"${MYSQL_ROOT_PASSWORD}" )
-			fi
-			
-			if [ "$SST_USER" -a "$SST_PASS" ]; then
-				echo "########## CREATING SST USER FOR REPLICATION ##########"
-				echo "CREATE USER '$SST_USER'@'%' IDENTIFIED BY '$SST_PASS' ;" | "${mysql[@]}"
-				echo "GRANT PROCESS, RELOAD, LOCK TABLES, REPLICATION CLIENT ON *.* TO '$SST_USER'@'%' ;" | "${mysql[@]}"
-				echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
-			fi
-
-			if [ "$MYSQL_DATABASE" ]; then
-				echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;" | "${mysql[@]}"
-				mysql+=( "$MYSQL_DATABASE" )
-			fi
-
-			if [ "$MYSQL_USER" -a "$MYSQL_PASSWORD" ]; then
-				echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" | "${mysql[@]}"
+					DELETE FROM mysql.user ;
+					CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
+					GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
+					DROP DATABASE IF EXISTS test ;
+					FLUSH PRIVILEGES ;
+				EOSQL
+				
+				if [ ! -z "$MYSQL_ROOT_PASSWORD" ]; then
+					mysql+=( -p"${MYSQL_ROOT_PASSWORD}" )
+				fi
+				
+				if [ "$SST_USER" -a "$SST_PASS" ]; then
+					echo "########## CREATING SST USER FOR REPLICATION ##########"
+					echo "CREATE USER '$SST_USER'@'%' IDENTIFIED BY '$SST_PASS' ;" | "${mysql[@]}"
+					echo "GRANT PROCESS, RELOAD, LOCK TABLES, REPLICATION CLIENT ON *.* TO '$SST_USER'@'%' ;" | "${mysql[@]}"
+					echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
+				fi
 
 				if [ "$MYSQL_DATABASE" ]; then
-					echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' ;" | "${mysql[@]}"
+					echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;" | "${mysql[@]}"
+					mysql+=( "$MYSQL_DATABASE" )
 				fi
 
-				echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
-			fi
+				if [ "$MYSQL_USER" -a "$MYSQL_PASSWORD" ]; then
+					echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" | "${mysql[@]}"
 
-			echo
-			for f in /docker-entrypoint-initdb.d/*; do
-				case "$f" in
-					*.sh)     echo "$0: running $f"; . "$f" ;;
-					*.sql)    echo "$0: running $f"; "${mysql[@]}" < "$f"; echo ;;
-					*.sql.gz) echo "$0: running $f"; gunzip -c "$f" | "${mysql[@]}"; echo ;;
-					*)        echo "$0: ignoring $f" ;;
-				esac
+					if [ "$MYSQL_DATABASE" ]; then
+						echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' ;" | "${mysql[@]}"
+					fi
+
+					echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
+				fi
+
 				echo
-			done
+				for f in /docker-entrypoint-initdb.d/*; do
+					case "$f" in
+						*.sh)     echo "$0: running $f"; . "$f" ;;
+						*.sql)    echo "$0: running $f"; "${mysql[@]}" < "$f"; echo ;;
+						*.sql.gz) echo "$0: running $f"; gunzip -c "$f" | "${mysql[@]}"; echo ;;
+						*)        echo "$0: ignoring $f" ;;
+					esac
+					echo
+				done
 
-			if ! kill -s TERM "$pid" || ! wait "$pid"; then
-				echo >&2 'MySQL init process failed.'
-				exit 1
+				if ! kill -s TERM "$pid" || ! wait "$pid"; then
+					echo >&2 'MySQL init process failed.'
+					exit 1
+				fi
+				mv /var/lib/mysql/mysql-bin.index /tmp
+				
+				echo
+				echo 'MySQL init process done. Ready for start up.'
+				echo
 			fi
-			mv /var/lib/mysql/mysql-bin.index /tmp
+
 			
-			echo
-			echo 'MySQL init process done. Ready for start up.'
-			echo
 		fi
 
 		chown -R mysql:mysql "$DATADIR"
@@ -164,6 +168,7 @@ elif [ ${CLUSTER} = "BOOTSTRAP" ]; then
     exec $@ --wsrep_new_cluster 
 else
     echo "########### Joining MariaDB cluster ${CLUSTER_NAME} on nodes ${CLUSTER}..."
+	initialize_db $@
 	cluster_conf
     exec $@ 
 fi
